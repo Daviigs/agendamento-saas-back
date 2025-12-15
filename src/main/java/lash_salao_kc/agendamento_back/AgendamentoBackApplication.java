fb28439eca04ad5fea4d3930bb0d3422dc8be935
Davi Miqueias <miqueiasdavi8@gmail.com>
2025-12-15 13:12:44 -0300

    first commit

---
+++ src/main/java/lash_salao_kc/agendamento_back/AgendamentoBackApplication.java
@@ -0,0 +1,13 @@
+package lash_salao_kc.agendamento_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lash_salao_kc/agendamento_back/AgendamentoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package lash_salao_kc.agendamento_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
